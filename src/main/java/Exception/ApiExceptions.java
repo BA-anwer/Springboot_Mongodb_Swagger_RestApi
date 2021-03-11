c4465ab6c72715943d256d36f7941bc38177a1d3
Ben Abdallah Anwer <anwer8322@gmail.com>
2021-03-11 20:05:42 +0100

    second commit

--- src/main/java/Exception/ApiExceptions.java
+++ src/main/java/Exception/ApiExceptions.java
@@ -1,8 +1,6 @@
 package Exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -12,7 +10,7 @@ public class ApiExceptions extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> globleExcpetionHandler(Exception ex, WebRequest request) {
-		return new ResponseEntity<Object>("succes test", null);
+		return new ResponseEntity<Object>("succes", null);
 	}
 
 
